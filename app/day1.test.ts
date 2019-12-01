1641cc236589444af2500a92bff40f3cb12d7bb7
David Whitney <david@davidwhitney.co.uk>
2019-12-01 16:53:34 +0100

    Day 1 complete.

--- app/day1.test.ts
+++ app/day1.test.ts
@@ -10,7 +10,15 @@ describe("Day 1", () => {
         expect(sut.calculate(100756)).toBe(33583);        
     });
 
-    it("Can solve puzzle input 1", async () => {
+    it("Can calculate fuel with fuel requirements needed for first and last example", () => {
+        const sut = new Day1();
+        
+        expect(sut.calculateWithFuel(14)).toBe(2);  
+        expect(sut.calculateWithFuel(1969)).toBe(966);    
+        expect(sut.calculateWithFuel(100756)).toBe(50346);        
+    });
+
+    it("Can solve puzzle 1", async () => {
         const testData: String = fs.readFileSync("./app/day1-input1.txt", "utf8");
         const dataItems = testData.split(require('os').EOL).map(asString => parseInt(asString));
 
@@ -18,5 +26,15 @@ describe("Day 1", () => {
         const result = sut.calculate(...dataItems);
 
         expect(result).toBe(3161483);
+    });
+    
+    it("Can solve puzzle 2", async () => {
+        const testData: String = fs.readFileSync("./app/day1-input1.txt", "utf8");
+        const dataItems = testData.split(require('os').EOL).map(asString => parseInt(asString));
+
+        const sut = new Day1();
+        const result = sut.calculateWithFuel(...dataItems);
+
+        expect(result).toBe(4739374);
     });
 });
--- app/day1.ts
+++ app/day1.ts
@@ -1,10 +1,27 @@
 export class Day1 {
-    calculate(...masses: number[]) {
+    public calculate(...masses: number[]): number {
         let total = 0;
         for(const mass of masses){
             const step1 = mass / 3;
             const step2 = Math.floor(step1);
             total += step2 - 2;
+        }
+        return total;
+    }
+
+    
+    public calculateWithFuel(...masses: number[]): number {
+        let total = 0;
+        for(const mass of masses){
+            const step1 = mass / 3;
+            const step2 = Math.floor(step1);
+            const cost = step2 - 2;
+
+            if (cost <= 0) {
+                return total;
+            }
+
+            total += cost + this.calculateWithFuel(cost);
         }
         return total;
     }
