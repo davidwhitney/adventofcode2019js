3acd5c7f21cafedefc19a16f48ccf511ef31e4b2
David Whitney <david@davidwhitney.co.uk>
2019-12-01 16:41:58 +0100

    Part 1 complete

--- app/day1.test.ts
+++ app/day1.test.ts
@@ -1,8 +1,22 @@
 import { Day1 } from "./day1";
 
+import * as fs from 'fs';
+
 describe("Day 1", () => {
-    it("???", () => {
+    it("Can calculate fuel needed for first and last example", () => {
         const sut = new Day1();
         
+        expect(sut.calculate(12)).toBe(2);  
+        expect(sut.calculate(100756)).toBe(33583);        
+    });
+
+    it("Can solve puzzle input 1", async () => {
+        const testData: String = fs.readFileSync("./app/day1-input1.txt", "utf8");
+        const dataItems = testData.split(require('os').EOL).map(asString => parseInt(asString));
+
+        const sut = new Day1();
+        const result = sut.calculate(...dataItems);
+
+        expect(result).toBe(3161483);
     });
 });
--- app/day1.ts
+++ app/day1.ts
@@ -1,2 +1,11 @@
 export class Day1 {
+    calculate(...masses: number[]) {
+        let total = 0;
+        for(const mass of masses){
+            const step1 = mass / 3;
+            const step2 = Math.floor(step1);
+            total += step2 - 2;
+        }
+        return total;
+    }
 }
